d464f9c0fc67dbb63e5957f8d7904e271b371a47
miwa-mngo24 <web.mango2020@gmail.com>
2024-09-11 23:55:15 +0900

    再提出

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,10 +1,12 @@
 // 変数の宣言
 let usertWeight;
 let usertHeight;
+let userBmi = usertWeight / (usertHeight * usertHeight);
 
 // 値の代入
 usertWeight = 68;
 usertHeight = 1.7;
+userBmi = 68 / (1.7 * 1.7);
 
 // 四則演算
-console.log(68 / (1.7 * 1.7));
+console.log(userBmi);
