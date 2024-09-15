e1b69573e6dc5aa0fef19e78ba78fd267224b387
miwa-mngo24 <web.mango2020@gmail.com>
2024-09-16 00:01:59 +0900

    DOM操作を使用して文字表示を変えよう_kadai_020　再提出

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,14 @@
 // btnというidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById("btn");
 
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById("text");
+
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener("click", () => {
-  //h2要素を「ボタンをクリックしました」というテキストに変更する
+  // h2要素を新しく作成する
+  const h2 = document.createElement("h2");
+
+  //作成したh2要素に「ボタンをクリックしました」というテキストを追加する
   text.textContent = "ボタンをクリックしました";
 });
